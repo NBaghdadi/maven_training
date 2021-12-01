4a1c09272463afb00cb30e55b08952259291e13d
Nidhal Baghdadi <baghdadi@et.esiea.fr>
2021-12-01 08:55:31 +0100

    Add live badges

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # maven_training
+![build CI](https://github.com/NBaghdadi/maven_training/actions/workflows/build.yml/badge.svg)     
+[![couverture par les tests](https://codecov.io/gh/NBaghdadi/maven_training/branch/main/graph/badge.svg?token=av2AvSf7SE)](https://codecov.io/gh/NBaghdadi/maven_training) 
+
+
--- src/test/java/fr/lernejo/SampleTest.java
+++ src/test/java/fr/lernejo/SampleTest.java
@@ -11,12 +11,15 @@ class SampleTest {
         int result = sample.op(Sample.Operation.ADD,5,5);
         Assertions.assertThat(result).as("5+5").isEqualTo(10);
 
+
+
     }
     @Test
     void op_MULT() {
         final Sample sample = new Sample();
         int result = sample.op(Sample.Operation.MULT,5,5);
         Assertions.assertThat(result).as("5*5").isEqualTo(25);
+
 
     }
 
@@ -27,7 +30,12 @@ class SampleTest {
         int result = sample.fact(5);
         Assertions.assertThat(result).as("fact(5)").isEqualTo(120);
 
-
+    }
+    @Test
+    void neg_fact() {
+        final Sample sample = new Sample();
+        int result = -1;
+        Assertions.assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(()->sample.fact(result));
 
     }
 
